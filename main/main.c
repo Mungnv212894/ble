a580701330531eb605a399abcd53d97a22f13383
mung2894 <vanmungxuanduong61@gmail.com>
2024-04-22 11:18:41 +0700

    Mô tả commit của bạn ở đây

--- main/main.c
+++ main/main.c
@@ -23,11 +23,8 @@ typedef enum {
 } app_gap_state_t;
 
 typedef struct {
-    bool dev_found;
     uint8_t bdname_len;
-    uint8_t eir_len;
     uint8_t rssi;
-    uint32_t cod;
     uint8_t eir[ESP_BT_GAP_EIR_DATA_LEN];
     uint8_t bdname[ESP_BT_GAP_MAX_BDNAME_LEN + 1];
     esp_bd_addr_t bda;
@@ -47,76 +44,17 @@ static char *bda2str(esp_bd_addr_t bda, char *str, size_t size) {
     return str;
 }
 
-static char *uuid2str(esp_bt_uuid_t *uuid, char *str, size_t size) {
-    if (uuid == NULL || str == NULL) {
-        return NULL;
-    }
-
-    if (uuid->len == 2 && size >= 5) {
-        snprintf(str, size, "%04x", uuid->uuid.uuid16);
-    } else if (uuid->len == 4 && size >= 9) {
-        snprintf(str, size, "%08"PRIx32, uuid->uuid.uuid32);
-    } else if (uuid->len == 16 && size >= 37) {
-        uint8_t *p = uuid->uuid.uuid128;
-        snprintf(str, size, "%02x%02x%02x%02x-%02x%02x-%02x%02x-%02x%02x-%02x%02x%02x%02x%02x%02x",
-                p[15], p[14], p[13], p[12], p[11], p[10], p[9], p[8],
-                p[7], p[6], p[5], p[4], p[3], p[2], p[1], p[0]);
-    } else {
-        return NULL;
-    }
-
-    return str;
-}
-
-static bool get_name_from_eir(uint8_t *eir, uint8_t *bdname, uint8_t *bdname_len) {
-    uint8_t *rmt_bdname = NULL;
-    uint8_t rmt_bdname_len = 0;
-
-    if (!eir) {
-        return false;
-    }
-
-    rmt_bdname = esp_bt_gap_resolve_eir_data(eir, ESP_BT_EIR_TYPE_CMPL_LOCAL_NAME, &rmt_bdname_len);
-    if (!rmt_bdname) {
-        rmt_bdname = esp_bt_gap_resolve_eir_data(eir, ESP_BT_EIR_TYPE_SHORT_LOCAL_NAME, &rmt_bdname_len);
-    }
-
-    if (rmt_bdname) {
-        if (rmt_bdname_len > ESP_BT_GAP_MAX_BDNAME_LEN) {
-            rmt_bdname_len = ESP_BT_GAP_MAX_BDNAME_LEN;
-        }
-
-        if (bdname) {
-            memcpy(bdname, rmt_bdname, rmt_bdname_len);
-            bdname[rmt_bdname_len] = '\0';
-        }
-        if (bdname_len) {
-            *bdname_len = rmt_bdname_len;
-        }
-        return true;
-    }
-
-    return false;
-}
-
 static void update_device_info(esp_bt_gap_cb_param_t *param) {
     char bda_str[18];
-    uint32_t cod = 0;
     int32_t rssi = -129; /* invalid value */
     uint8_t *bdname = NULL;
     uint8_t bdname_len = 0;
-    uint8_t *eir = NULL;
-    uint8_t eir_len = 0;
     esp_bt_gap_dev_prop_t *p;
 
     ESP_LOGI(GAP_TAG, "Device found: %s", bda2str(param->disc_res.bda, bda_str, 18));
     for (int i = 0; i < param->disc_res.num_prop; i++) {
         p = param->disc_res.prop + i;
         switch (p->type) {
-        case ESP_BT_GAP_DEV_PROP_COD:
-            cod = *(uint32_t *)(p->val);
-            ESP_LOGI(GAP_TAG, "--Class of Device: 0x%"PRIx32, cod);
-            break;
         case ESP_BT_GAP_DEV_PROP_RSSI:
             rssi = *(int8_t *)(p->val);
             ESP_LOGI(GAP_TAG, "--RSSI: %"PRId32, rssi);
@@ -126,8 +64,7 @@ static void update_device_info(esp_bt_gap_cb_param_t *param) {
             bdname = (uint8_t *)(p->val);
             break;
         case ESP_BT_GAP_DEV_PROP_EIR: {
-            eir_len = p->len;
-            eir = (uint8_t *)(p->val);
+            memcpy(m_dev_info.eir, p->val, p->len);
             break;
         }
         default:
@@ -135,54 +72,23 @@ static void update_device_info(esp_bt_gap_cb_param_t *param) {
         }
     }
 
-    /* search for device with Major device type "PHONE" or "Audio/Video" in COD */
-    app_gap_cb_t *p_dev = &m_dev_info;
-    if (p_dev->dev_found) {
-        return;
-    }
-
-    if (!esp_bt_gap_is_valid_cod(cod) ||
-	    (!(esp_bt_gap_get_cod_major_dev(cod) == ESP_BT_COD_MAJOR_DEV_PHONE) &&
-             !(esp_bt_gap_get_cod_major_dev(cod) == ESP_BT_COD_MAJOR_DEV_AV))) {
-        return;
-    }
-
-    memcpy(p_dev->bda, param->disc_res.bda, ESP_BD_ADDR_LEN);
-    p_dev->dev_found = true;
-
-    p_dev->cod = cod;
-    p_dev->rssi = rssi;
+    // Display MAC address and device name
+    ESP_LOGI(GAP_TAG, "MAC Address: %s", bda2str(param->disc_res.bda, bda_str, 18));
     if (bdname_len > 0) {
-        memcpy(p_dev->bdname, bdname, bdname_len);
-        p_dev->bdname[bdname_len] = '\0';
-        p_dev->bdname_len = bdname_len;
+        memcpy(m_dev_info.bdname, bdname, bdname_len);
+        m_dev_info.bdname[bdname_len] = '\0';
+        ESP_LOGI(GAP_TAG, "Device Name: %s", m_dev_info.bdname);
+    } else {
+        ESP_LOGI(GAP_TAG, "Device Name: N/A");
     }
-    if (eir_len > 0) {
-        memcpy(p_dev->eir, eir, eir_len);
-        p_dev->eir_len = eir_len;
-    }
-
-    if (p_dev->bdname_len == 0) {
-        get_name_from_eir(p_dev->eir, p_dev->bdname, &p_dev->bdname_len);
-    }
-
-    ESP_LOGI(GAP_TAG, "Found a target device, address %s, name %s", bda_str, p_dev->bdname);
-    p_dev->state = APP_GAP_STATE_DEVICE_DISCOVER_COMPLETE;
-    ESP_LOGI(GAP_TAG, "Cancel device discovery ...");
-    esp_bt_gap_cancel_discovery();
 }
 
 static void bt_app_gap_init(void) {
-    app_gap_cb_t *p_dev = &m_dev_info;
-    memset(p_dev, 0, sizeof(app_gap_cb_t));
-    p_dev->state = APP_GAP_STATE_IDLE;
+    memset(&m_dev_info, 0, sizeof(app_gap_cb_t));
+    m_dev_info.state = APP_GAP_STATE_IDLE;
 }
 
 static void bt_app_gap_cb(esp_bt_gap_cb_event_t event, esp_bt_gap_cb_param_t *param) {
-    app_gap_cb_t *p_dev = &m_dev_info;
-    char bda_str[18];
-    char uuid_str[37];
-
     switch (event) {
     case ESP_BT_GAP_DISC_RES_EVT: {
         update_device_info(param);
@@ -191,40 +97,18 @@ static void bt_app_gap_cb(esp_bt_gap_cb_event_t event, esp_bt_gap_cb_param_t *pa
     case ESP_BT_GAP_DISC_STATE_CHANGED_EVT: {
         if (param->disc_st_chg.state == ESP_BT_GAP_DISCOVERY_STOPPED) {
             ESP_LOGI(GAP_TAG, "Device discovery stopped.");
-            if ((p_dev->state == APP_GAP_STATE_DEVICE_DISCOVER_COMPLETE ||
-                 p_dev->state == APP_GAP_STATE_DEVICE_DISCOVERING) && p_dev->dev_found) {
-                p_dev->state = APP_GAP_STATE_SERVICE_DISCOVERING;
-                ESP_LOGI(GAP_TAG, "Discover services ...");
-                esp_bt_gap_get_remote_services(p_dev->bda);
-            }
         } else if (param->disc_st_chg.state == ESP_BT_GAP_DISCOVERY_STARTED) {
             ESP_LOGI(GAP_TAG, "Discovery started.");
         }
         break;
     }
-    case ESP_BT_GAP_RMT_SRVCS_EVT: {
-        if (memcmp(param->rmt_srvcs.bda, p_dev->bda, ESP_BD_ADDR_LEN) == 0 &&
-            p_dev->state == APP_GAP_STATE_SERVICE_DISCOVERING) {
-            p_dev->state = APP_GAP_STATE_SERVICE_DISCOVER_COMPLETE;
-            if (param->rmt_srvcs.stat == ESP_BT_STATUS_SUCCESS) {
-                ESP_LOGI(GAP_TAG, "Services for device %s found", bda2str(p_dev->bda, bda_str, 18));
-                for (int i = 0; i < param->rmt_srvcs.num_uuids; i++) {
-                    esp_bt_uuid_t *u = param->rmt_srvcs.uuid_list + i;
-                    ESP_LOGI(GAP_TAG, "--%s", uuid2str(u, uuid_str, 37));
-                }
-            } else {
-                ESP_LOGI(GAP_TAG, "Services for device %s not found", bda2str(p_dev->bda, bda_str, 18));
-            }
-        }
-        break;
-    }
+    case ESP_BT_GAP_RMT_SRVCS_EVT:
     case ESP_BT_GAP_RMT_SRVC_REC_EVT:
     default: {
         ESP_LOGI(GAP_TAG, "event: %d", event);
         break;
     }
     }
-    return;
 }
 
 static void bt_app_gap_start_up(void *pvParameter) {
@@ -243,7 +127,6 @@ static void bt_app_gap_start_up(void *pvParameter) {
 
         /* start to discover nearby Bluetooth devices */
         app_gap_cb_t *p_dev = &m_dev_info;
-        p_dev->state = APP_GAP_STATE_DEVICE_DISCOVERING;
         esp_bt_gap_start_discovery(ESP_BT_INQ_MODE_GENERAL_INQUIRY, 10, 0);
 
         /* Delay for a certain amount of time before restarting the search */
